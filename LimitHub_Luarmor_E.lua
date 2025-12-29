64784b74c25c35c5780d1a3f11360f1d6ce3a755
FakeModz0 <yhandygemranalan@gmail.com>
2025-12-29 22:18:07 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub_Luarmor_E.lua
+++ LimitHub_Luarmor_E.lua
@@ -1,3 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
-ID="ca206290-bea6-42a9-b475-6bdbf4aca83c";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/2ZjMtj5l/raw"))()
